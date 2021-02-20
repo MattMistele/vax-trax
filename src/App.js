582246a4c2daa055f04b143d39cf2151ef016a7b
Patrick <patricklee260@gmail.com>
2021-02-20 15:47:42 -0800

    This is a test comment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //This is a comment from Patrick.
   return (
     <div className="App">
       <header className="App-header">
