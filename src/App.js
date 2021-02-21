bf8c8d19de637c70a2cd00efdf245df25110be52
Patrick <patricklee260@gmail.com>
2021-02-20 17:59:04 -0800

    Added a Title Link

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
-  //This is a comment from Patrick.
-  //Hello this is manasvini
-  //reee it grace
-  //mubashir
-  //yo this is manasvini
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact />
+        </Switch>
+      </Router>  
+    </>
   );
 }
 
